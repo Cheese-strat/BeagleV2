5f8082174b290fa5bb7ee2c466af718a54a89f62
DESKTOP-R95DHKV\ben <benjaminjgiles@gmail.com>
2022-04-22 19:42:21 +0800

    Ping timing into positives

--- src/Commmands/ping.ts
+++ src/Commmands/ping.ts
@@ -3,18 +3,16 @@ import Command from "src/Structures/Command";
 
 const cmd: Command = {
 	displayName: "Ping",
-	build: new SlashCommandBuilder()
-		.setName("ping")
-		.setDescription("Pings the bot and the discord API to test reaction time and latency"),
+	build: new SlashCommandBuilder().setName("ping").setDescription("Pings the bot and the discord API to test reaction time and latency"),
 	cooldown: 2,
 	async execute(interaction) {
 		interaction.reply("pinging...");
 		const message = await interaction.fetchReply();
 		let ping: number;
 		if ("createdTimestamp" in message) {
-			ping = interaction.createdTimestamp - message.createdTimestamp;
+			ping = message.createdTimestamp - interaction.createdTimestamp;
 		} else {
-			ping = interaction.createdTimestamp - Number(message.timestamp);
+			ping = Number(message.timestamp) - interaction.createdTimestamp;
 		}
 		interaction.editReply(`Pong! Latency is ${ping}ms.`);
 
