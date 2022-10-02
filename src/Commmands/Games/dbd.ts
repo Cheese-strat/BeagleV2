1c20ce924613bee40608e22f7db75abfeb4d5d8d
DESKTOP-R95DHKV\ben <benjaminjgiles@gmail.com>
2022-10-02 20:50:59 +0800

    Dbd shrine command working

--- src/Commmands/Games/dbd.ts
+++ src/Commmands/Games/dbd.ts
@@ -1,7 +1,10 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
+import { AttachmentBuilder, ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 import Command from "src/Structures/Command";
 import axios from "axios";
+import Jimp from "jimp";
+import config from "../../../config.json";
+import { dbd } from "./game-data.json";
 const api_base = `https://dbd.tricky.lol/api/`;
 
 const cmd: Command = {
@@ -22,43 +25,60 @@ const cmd: Command = {
 				return;
 			}
 			const shrine_res: shrine_result = response.data;
-			let fields = [];
-			for (let perkData of shrine_res.perks) {
-				let field = {
-					name: "",
-					value: "",
-					inline: true,
-				};
 
-				const response = await axios.get(`${api_base}perkinfo?perk=${perkData.id}`).catch(error => {
-					console.error(error);
-				});
-
-				if (!response) {
-					console.log("Axios request returned void");
-					return;
+			let perks = shrine_res.perks.map(({ id }, index) => {
+				///@ts-ignore
+				let perkData = dbd.perks.find(element => element.api_name === id); // as perkInfo_result;
+				if (!perkData) {
+					console.log(`Perk ID not found: ${id}`);
+					return false;
 				}
-				const perkInfo_res: perkInfo_result = response.data;
 
-				field.name = perkInfo_res.name;
-				field.value = `${perkInfo_res.role} perk`;
-				fields.push(field);
-
-				
-			}
-			fields.splice(2, 0, {
-				name: "\u200B",
-				value: "\u200B",
-				inline: false,
+				//"R:\\Programming\\testing-and-scripts\\PerkIcons\\IconPerks_scourgeHookFloodsOfRage.png"
+				let value = {
+					icon_path: config.shrine_img_paths.icon_folder + perkData.path,
+					//gets written later
+					icon: undefined,
+					x: 0,
+					y: 0,
+					perkname: perkData.name,
+					text: "",
+				};
+				if (index === 0) {
+					value.x = 20;
+					value.y = 20;
+				}
+				if (index === 1) {
+					value.x = 20;
+					value.y = 650;
+				}
+				if (index === 2) {
+					value.x = 1500;
+					value.y = 20;
+				}
+				if (index === 3) {
+					value.x = 1500;
+					value.y = 650;
+				}
+				return value;
 			});
+			if (!isPerksArray(perks)) {
+				await interaction.editReply("Sorry there was a problem with the command");
+				return;
+			}
 			const ShrineEmbed = new EmbedBuilder()
 				.setTitle("Current Shrine Rotation")
-				.addFields(fields)
 				.setFooter({ text: `The shrine will reset at: ` })
-				.setTimestamp(shrine_res.end);
-				
-
-			await interaction.editReply({ embeds: [ShrineEmbed] });
+				.setTimestamp(shrine_res.end * 1000);
+			const shrine_img = await MakeShrineImage(config.shrine_img_paths, perks[0], perks[1]!, perks[2]!, perks[3]!);
+			//Saves the image into the file system
+			await shrine_img.writeAsync(config.shrine_img_paths.end_path);
+			const file = new AttachmentBuilder("./current_shrine.png");
+			ShrineEmbed.setImage("attachment://current_shrine.png");
+			await interaction.editReply({
+				embeds: [ShrineEmbed],
+				files: [file],
+			});
 			return;
 		}
 		interaction.reply(`a biblioteca é engraçada.`);
@@ -94,15 +114,160 @@ interface shrine_result {
 	start: number;
 	end: number;
 }
-interface perkInfo_result {
-	id: string;
-	categories: string[];
-	name: string;
-	description: string;
-	role: string;
-	character: number;
-	tunables: [string, string, string][];
-	modifier: string;
-	teachable: number;
-	image: string;
+// interface perkInfo_result {
+// 	name: string;
+// 	description: string;
+// 	isSurvivor: boolean;
+// 	character: number;
+// 	api_name: string;
+// 	path: string;
+// }
+interface PathsOBJ {
+	background: string;
+	Boldfont: string;
+	lightfont: string;
+	perkIcon_bg: string;
+	end_path: string;
+}
+interface perk {
+	icon_path: string;
+	icon: undefined | Jimp;
+	x: number;
+	y: number;
+	perkname: string;
+	text: string;
+}
+function isPerksArray(array: (perk | false)[]): array is perk[] {
+	return !array.includes(false);
+}
+async function MakeShrineImage(bg_paths: PathsOBJ, perk1: perk, perk2: perk, perk3: perk, perk4: perk): Promise<Jimp> {
+	const background_image = await Jimp.read(bg_paths.background);
+	const Boldfont = await Jimp.loadFont(bg_paths.Boldfont);
+	const lightfont = await Jimp.loadFont(bg_paths.lightfont);
+	// reads the watermark image
+	let perk_icon_bg = await Jimp.read(bg_paths.perkIcon_bg);
+	// resizes the watermark image
+	perk_icon_bg = await perk_icon_bg.resize(400, 400);
+	// reads the image
+
+	//perk1 top left
+	perk1.icon = await Jimp.read(perk1.icon_path);
+	background_image.print(Boldfont, perk1.x + 220, perk1.y, perk1.perkname);
+	if (perk1.text !== "") background_image.print(lightfont, perk1.x + 270, perk1.y + 50, perk1.text);
+
+	background_image.composite(perk_icon_bg, perk1.x, perk1.y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacityDest: 1,
+		opacitySource: 1,
+	});
+
+	perk1.icon = await perk1.icon.resize(400, 400);
+	background_image.composite(perk1.icon, perk1.x, perk1.y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacityDest: 1,
+		opacitySource: 1,
+	});
+
+	//perk2 bottom left
+	perk2.icon = await Jimp.read(perk2.icon_path);
+	background_image.print(Boldfont, perk2.x + 220, perk2.y, perk2.perkname);
+	if (perk2.text !== "") background_image.print(lightfont, perk2.x + 270, perk2.y + 50, perk2.text);
+
+	background_image.composite(perk_icon_bg, perk2.x, perk2.y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacityDest: 1,
+		opacitySource: 1,
+	});
+
+	perk2.icon = await perk2.icon.resize(400, 400);
+	background_image.composite(perk2.icon, perk2.x, perk2.y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacityDest: 1,
+		opacitySource: 1,
+	});
+
+	//perk3 top right
+	perk3.icon = await Jimp.read(perk3.icon_path);
+	background_image.print(
+		Boldfont,
+		0,
+		0,
+		{
+			text: perk3.perkname,
+			alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT,
+			alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM,
+		},
+		perk3.x + 100,
+		perk3.y + 350,
+	);
+	if (perk3.text !== "")
+		background_image.print(
+			lightfont,
+			0,
+			0,
+			{
+				text: perk3.text,
+				alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT,
+				alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM,
+			},
+			perk3.x + 150,
+			perk3.y + 400,
+		);
+
+	background_image.composite(perk_icon_bg, perk3.x, perk3.y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacityDest: 1,
+		opacitySource: 1,
+	});
+
+	perk3.icon = await perk3.icon.resize(400, 400);
+	background_image.composite(perk3.icon, perk3.x, perk3.y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacityDest: 1,
+		opacitySource: 1,
+	});
+
+	//perk4 bottom right
+	perk4.icon = await Jimp.read(perk4.icon_path);
+	background_image.print(
+		Boldfont,
+		0,
+		0,
+		{
+			text: perk4.perkname,
+			alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT,
+			alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM,
+		},
+		perk4.x + 100,
+		perk4.y + 350,
+	);
+
+	if (perk4.text !== "")
+		background_image.print(
+			lightfont,
+			0,
+			0,
+			{
+				text: perk4.text,
+				alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT,
+				alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM,
+			},
+			perk4.x + 150,
+			perk4.y + 400,
+		);
+
+	background_image.composite(perk_icon_bg, perk4.x, perk4.y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacityDest: 1,
+		opacitySource: 1,
+	});
+
+	perk4.icon = await perk4.icon.resize(400, 400);
+	background_image.composite(perk4.icon, perk4.x, perk4.y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacityDest: 1,
+		opacitySource: 1,
+	});
+
+	return background_image;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import BeagleClient from "./Structures/Client";
-import IntentHelper from "./Structures/Helpers/IntentHelper"
+import IntentHelper from "./Structures/Helpers/IntentHelper";
 import { client_Options } from "../config.json";
-import path from "path"
+import path from "path";
 
 const client = new BeagleClient(IntentHelper(client_Options), path.resolve(__dirname));
 
 console.log(`initializing Discord bot`);
 
-// THIS IS REQUIRED. Send raw events to Erela.js
+//REQUIRED. Send raw events to Erela.js
 client.on("raw", d => client.music.updateVoiceState(d));
 Error.stackTraceLimit = Infinity;
 client.startup();
